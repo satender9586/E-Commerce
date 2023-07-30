6d43ac8ccf9871a6d3e66b784905539c36ad8f50
jai singh <jaibhandari804@gmail.com>
2023-07-30 17:10:55 +0530

    dotenv-added

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express()
+const dotenv=require("dotenv")
 
+dotenv.config();
 const PORT = 9089;
+
+console.log()
 
 app.get("/", (req, res) => {
     console.log("Hello World")
 })
 
-app.listen(PORT, (req, res) => {
-    console.log("Server Is Runnin on ", PORT)
+app.listen((process.env.PORT_NUMBER||PORT), (req, res) => {
+    console.log("Server Is Runnin on ", process.env.PORT_NUMBER||PORT)
 })
