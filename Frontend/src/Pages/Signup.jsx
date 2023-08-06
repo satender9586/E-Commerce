2cf07283be00ead3ed7f88c48c62cb8fd1411127
Satender Ahirwar <sksatenderkumar1693@gmail.com>
2023-08-06 09:12:24 +0530

    validation Sign From

--- Frontend/src/Pages/Signup.jsx
+++ Frontend/src/Pages/Signup.jsx
@@ -1,28 +1,53 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../Components/Layout/Layout'
-import { useDisclosure, Button, Input } from '@chakra-ui/react'
-import {
-    FormControl,
-    FormLabel,
-    FormErrorMessage,
-    FormHelperText,
-} from '@chakra-ui/react'
-import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
+import { useDisclosure, Button, Input, InputGroup, InputRightAddon, Text } from '@chakra-ui/react'
+import { BiSolidHide, BiShow } from "react-icons/bi"
+import { SinginputValidation } from '../Validation/validationFuncltion'
+import { FormControl, FormLabel, FormErrorMessage, FormHelperText, } from '@chakra-ui/react'
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, } from '@chakra-ui/react'
 
-} from '@chakra-ui/react'
 
 const Signup = () => {
 
+    const [formData, setFormData] = useState({ name: "", phone: "", email: "", password: "", repassword: "", address: "" })
+    const [showPassword, setShowPassword] = useState(false)
+    const [repassword, setrepassword] = useState(false)
+    const [formError, setFormError] = useState({})
+    const [submitem, setFormSubmit] = useState(false)
 
 
+    // form clear function 
 
+    const formclear = () => {
+        setFormData({ name: "", phone: "", email: "", password: "", repassword: "", address: "" })
+        setFormError({})
+    }
+
+    // password Hide & Show Function
+    const PasswordShow = () => { setShowPassword(!showPassword) }
+    const Reenterpassword = () => { setrepassword(!repassword) }
+
+    // Input take form
+    const handlerChange = (e) => {
+        const { name, value } = e.target;
+        const newoformobj = { ...formData, [name]: value }
+        setFormData({ ...newoformobj })
+
+        if (submitem) {
+            const errorform = SinginputValidation(newoformobj)
+            setFormError(errorform)
+        }
+
+    }
+    // form Data collected
+    const submitFormHandler = (e) => {
+        e.preventDefault()
+        setFormSubmit(true)
+
+        const finalvalidation = SinginputValidation(formData)
+        setFormError(finalvalidation)
+
+    }
 
     // modal open
     useEffect(() => {
@@ -41,36 +66,52 @@ const Signup = () => {
                         <ModalHeader>Register User</ModalHeader>
                         <ModalCloseButton />
                         <ModalBody>
-                            <form>
+                            <form >
                                 <FormControl>
                                     <FormLabel>Full Name</FormLabel>
-                                    <Input type='text' />
+                                    <Input value={formData.name} name="name" onChange={handlerChange} type='text' />
+                                    <Text color={"Red"} fontSize={"12px"}>{formError.name}</Text>
                                 </FormControl>
                                 <FormControl>
                                     <FormLabel>Email Id</FormLabel>
-                                    <Input type='text' />
+                                    <Input value={formData.email} name="email" onChange={handlerChange} type='email' />
+                                    <Text color={"Red"} fontSize={"12px"}>{formError.email}</Text>
                                 </FormControl>
                                 <FormControl>
                                     <FormLabel>Phone Number</FormLabel>
-                                    <Input type='number' />
+                                    <Input value={formData.phone} name="phone" onChange={handlerChange} type='number' />
+                                    <Text color={"Red"} fontSize={"12px"}>{formError.phone}</Text>
                                 </FormControl>
                                 <FormControl>
                                     <FormLabel>Enter Password</FormLabel>
-                                    <Input type='text' />
+                                    <InputGroup >
+                                        <Input value={formData.password} onChange={handlerChange} name="password" type={showPassword ? "text" : 'password'} />
+                                        <InputRightAddon padding={"0"} children={<Button w={"100%"} variant={"none"} onClick={PasswordShow}>{showPassword ? <BiShow /> : <BiSolidHide />}</Button>} />
+
+                                    </InputGroup>
+                                    <Text color={"Red"} fontSize={"12px"}>{formError.password}</Text>
                                 </FormControl>
                                 <FormControl>
                                     <FormLabel>Re-Enter Password</FormLabel>
-                                    <Input type='text' />
+                                    <InputGroup>
+                                        <Input value={formData.repassword} onChange={handlerChange} name="repassword" type={repassword ? "text" : 'password'} />
+                                        <InputRightAddon padding={"0"} children={<Button w={"100%"} variant={"none"} onClick={Reenterpassword}>{repassword ? <BiShow /> : <BiSolidHide />}</Button>} />
+
+                                    </InputGroup>
+                                    <Text color={"Red"} fontSize={"12px"}>{formError.repassword}</Text>
                                 </FormControl>
                                 <FormControl>
                                     <FormLabel>Address </FormLabel>
-                                    <Input type='text' />
+                                    <Input value={formData.address} name="address" onChange={handlerChange} type='text' />
                                 </FormControl>
                             </form>
                         </ModalBody>
 
                         <ModalFooter>
-                            <Button colorScheme='blue' mr={3} onClick={onClose}>
+                            <Button colorScheme='red' mr={3} onClick={formclear} >
+                                Clear
+                            </Button>
+                            <Button colorScheme='blue' mr={3} onClick={submitFormHandler} >
                                 Save
                             </Button>
 
---
+++ Frontend/src/Validation/validationFuncltion.jsx
@@ -0,0 +1,36 @@
+export const SinginputValidation = (data) => {
+    const error = {};
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z].*[a-z]).{8,20}$/;
+    const emailRegex = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+    const phoneRegex = /^\d{10}$/;
+
+    if (!data.name) {
+        error.name = "Name is Required";
+    }
+
+    if (!data.email) {
+        error.email = "Email is Required";
+    } else if (!emailRegex.test(data.email)) {
+        error.email = "Invalid Email";
+    }
+
+    if (!data.password) {
+        error.password = "Password is Required";
+    } else if (!passwordRegex.test(data.password)) {
+        error.password = "Invalid Password";
+    }
+
+    if (!data.repassword) {
+        error.repassword = "Please re-enter your password";
+    } else if (data.password !== data.repassword) {
+        error.repassword = "Passwords do not match";
+    }
+
+    if (!data.phone) {
+        error.phone = "Phone Number is Required";
+    } else if (!phoneRegex.test(data.phone)) {
+        error.phone = "Invalid Phone Number";
+    }
+
+    return error;
+};
